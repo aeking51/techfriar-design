e895b1e034802dba3b1336511ae9316c728dc2f8
Jerin <83822886+aeking51@users.noreply.github.com>
2023-09-02 15:20:32 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,7 @@
 const showPassword = document.querySelector("#showPassword");
 const password = document.querySelector("#password");
 
-const showConfirmPassword = document.querySelector("#showConfirmPassword");
-const ConfirmPassword = document.querySelector("#confirm-password");  
 
-showConfirmPassword.addEventListener("click", function () {
-    //Toggling the eye icon
-    this.classList.toggle("fa-eye");
-    // toggle the type attribute
-    const type = ConfirmPassword.getAttribute("type") === "password" ? "text" : "password";
-    ConfirmPassword.setAttribute("type", type);
-    
-});
 
 
 showPassword.addEventListener("click", function () {
@@ -22,5 +12,18 @@ showPassword.addEventListener("click", function () {
     // toggle the type attribute
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
+    
+});
+
+const showConfirmPassword = document.querySelector("#showConfirmPassword");
+const ConfirmPassword = document.querySelector("#confirm-password");  
+
+showConfirmPassword.addEventListener("click", function () {
+    console.log(1)
+    //Toggling the eye icon
+    this.classList.toggle("fa-eye");
+    // toggle the type attribute
+    const type = ConfirmPassword.getAttribute("type") === "password" ? "text" : "password";
+    ConfirmPassword.setAttribute("type", type);
     
 });
